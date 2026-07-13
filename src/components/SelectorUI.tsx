c56bd1a44b3f4c8fac7e29dbc60beadc2176fde7
Domaalva <139288690+Domaalva@users.noreply.github.com>
2026-07-13 18:41:10 +0000

    Cambios

--- src/components/SelectorUI.tsx
+++ src/components/SelectorUI.tsx
@@ -1,31 +1,51 @@
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
+import { useState } from "react";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import type { SelectChangeEvent } from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Typography from "@mui/material/Typography";
 
 export default function SelectorUI() {
+  const [cityInput, setCityInput] = useState("");
+
   const handleChange = (event: SelectChangeEvent<string>) => {
-    alert(event.target.value);
+    setCityInput(event.target.value);
   };
 
   return (
-    <FormControl fullWidth>
-      <InputLabel id="city-select-label">Ciudad</InputLabel>
+    <>
+      <FormControl fullWidth>
+        <InputLabel id="city-select-label">
+          Ciudad
+        </InputLabel>
 
-      <Select
-        labelId="city-select-label"
-        id="city-simple-select"
-        label="Ciudad"
-        onChange={handleChange}
-      >
-        <MenuItem disabled>
-          <em>Seleccione una ciudad</em>
-        </MenuItem>
-        <MenuItem value="guayaquil">Guayaquil</MenuItem>
-        <MenuItem value="quito">Quito</MenuItem>
-        <MenuItem value="manta">Manta</MenuItem>
-        <MenuItem value="cuenca">Cuenca</MenuItem>
-      </Select>
-    </FormControl>
+        <Select
+          labelId="city-select-label"
+          id="city-simple-select"
+          label="Ciudad"
+          value={cityInput}
+          onChange={handleChange}
+        >
+          <MenuItem value="" disabled>
+            <em>Seleccione una ciudad</em>
+          </MenuItem>
+
+          <MenuItem value="guayaquil">Guayaquil</MenuItem>
+          <MenuItem value="quito">Quito</MenuItem>
+          <MenuItem value="manta">Manta</MenuItem>
+          <MenuItem value="cuenca">Cuenca</MenuItem>
+        </Select>
+      </FormControl>
+
+      {cityInput && (
+        <Typography sx={{ mt: 2 }}>
+          Información del clima en{" "}
+          <strong>
+            {cityInput.charAt(0).toUpperCase() + cityInput.slice(1)}
+          </strong>
+        </Typography>
+      )}
+    </>
   );
 }
