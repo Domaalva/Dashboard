f7a1f669a5cf9329b428e5ebc94fcf0287d60b45
Domaalva <139288690+Domaalva@users.noreply.github.com>
2026-07-20 18:52:44 +0000

    guia 17

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -5,10 +5,10 @@ const CITY_COORDS: Record<
   string,
   { latitude: number; longitude: number }
 > = {
-  Guayaquil: { latitude: -2.1962, longitude: -79.8862 },
-  Quito: { latitude: -0.22, longitude: -78.51 },
-  Manta: { latitude: -0.96, longitude: -80.73 },
-  Cuenca: { latitude: -2.9, longitude: -78.98 },
+  guayaquil: { latitude: -2.1962, longitude: -79.8862 },
+  quito: { latitude: -0.22, longitude: -78.51 },
+  manta: { latitude: -0.96, longitude: -80.73 },
+  cuenca: { latitude: -2.9, longitude: -78.98 },
 };
 
 export default function useFetchData(
@@ -19,10 +19,15 @@ export default function useFetchData(
 
   useEffect(() => {
 
-    const cityConfig =
-      selectedOption !== null
-        ? CITY_COORDS[selectedOption]
-        : CITY_COORDS["Guayaquil"];
+    const cityKey = selectedOption ?? "guayaquil";
+
+    const cityConfig = CITY_COORDS[cityKey];
+
+    if (!cityConfig) {
+      console.error("Ciudad no encontrada:", cityKey);
+      setData(null);
+      return;
+    }
 
     const URL = `https://api.open-meteo.com/v1/forecast?latitude=${cityConfig.latitude}&longitude=${cityConfig.longitude}&current=temperature_2m,apparent_temperature,relative_humidity_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`;
 
@@ -31,7 +36,7 @@ export default function useFetchData(
         const response = await fetch(URL);
 
         if (!response.ok) {
-          throw new Error("Error al obtener datos del clima");
+          throw new Error("Error en la petición");
         }
 
         const json: OpenMeteoResponse = await response.json();
